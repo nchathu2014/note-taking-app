51dd4fed42c04a8f0bf351b1ca69f840a333618b
nchathu2014 <nchathu2014@gmail.com>
2026-04-24 19:52:39 -0400

    cached mongoose

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,33 @@
 import mongoose from "mongoose";
-const MONGODB_URI = process.env.NEXT_MONGODB_URI || '';
+
+declare global {
+  var mongoose:
+    | {
+        conn: mongoose.Mongoose | null;
+        promise: Promise<mongoose.Mongoose> | null;
+      }
+    | undefined;
+}
+
+const MONGODB_URI = process.env.NEXT_MONGODB_URI || "";
+
+let cached = global.mongoose ?? { conn: null, promise: null };
 
 export async function dbConnect() {
   try {
-    const db = await mongoose.connect(MONGODB_URI);
-    console.log("Connected to mongodb");
+    if (cached.conn) {
+      console.log("Using existing mongodb connection");
+      return cached.conn;
+    }
+
+    if (!cached.promise) {
+      cached.promise = mongoose.connect(MONGODB_URI);
+    }
+
+    cached.conn = await cached.promise;
+    global.mongoose = cached;
+    console.log("New mongodb connection established");
+    return cached.conn;
   } catch (error) {
     console.error("Failed to connect mongodb", error);
     throw error;
