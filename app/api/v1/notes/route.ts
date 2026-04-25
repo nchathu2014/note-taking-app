ed82ca2efe9c6dea2c1440545b365655bbd8d683
nchathu2014 <nchathu2014@gmail.com>
2026-04-25 00:48:35 -0400

    get all notes operation

--- app/api/v1/notes/route.ts
+++ app/api/v1/notes/route.ts
@@ -2,6 +2,33 @@ import { dbConnect } from "@/lib/back_db";
 import { Note } from "@/models/Note";
 import { NextRequest, NextResponse } from "next/server";
 
+// Fetch All Notes
+export async function GET(request: NextRequest) {
+  try {
+    await dbConnect();
+    const notes = await Note.find();
+    return NextResponse.json({
+      status: "success",
+      data: {
+        notes,
+        total: notes?.length,
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        status: "error",
+        data: {
+          message:
+            error instanceof Error ? error?.message : "Something went wrong",
+        },
+      },
+      { status: 500 },
+    );
+  }
+}
+
+//Create a Note
 export async function POST(request: NextRequest) {
   try {
     await dbConnect();
@@ -23,8 +50,8 @@ export async function POST(request: NextRequest) {
       {
         status: "error",
         data: {
-          message: "Something went wrong",
-          errorStackTrace: error,
+          message:
+            error instanceof Error ? error.message : "Something went wrong",
         },
       },
       { status: 500 },
