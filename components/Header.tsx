f922676cb6a506d7fe1521047a2f9dfc941093e1
nchathu2014 <nchathu2014@gmail.com>
2026-04-27 14:17:00 -0400

    mobile friendly fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,13 @@ export function Header() {
     <nav className="bg-gray-50 shadow-sm border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center p-3">
-          <h1 className="text-2xl md:text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            <Link href={BASE_URL}>Note Taking App</Link>
+          <h1>
+            <Link
+              href={BASE_URL}
+              className="text-2xl md:text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50"
+            >
+              Note Taking App
+            </Link>
           </h1>
           <span className="text-sm md:text-lg font-normal">
             Built with: Next.JS{" "}
