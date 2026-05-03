68db02eed42620362202b3718f51616e1e0819e5
nchathu2014 <nchathu2014@gmail.com>
2026-05-03 12:52:53 -0400

    Add a slogan

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,19 @@ export function Header() {
   return (
     <nav className="bg-gray-50 shadow-sm border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center p-3">
+        <div className="flex flex-col justify-center md:flex-row md:justify-between items-center p-3">
           <h1>
             <Link
               href={BASE_URL}
               className="text-2xl md:text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50"
             >
-              Note Taking App
+              <span className="text-red-600">N</span>
+              <span className="text-gray-400">o<span className="text-black">t</span>o</span>
+              
             </Link>
+            <div className="text-sm">
+From thought to note in seconds
+            </div>
           </h1>
           <span className="text-sm md:text-lg font-normal">
             Built with: Next.JS{" "}
